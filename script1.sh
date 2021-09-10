1fe5a1cf15639072a457f98e7d06921f0a3e87c3
Adam Lewison <lwsada002@cs.uct.ac.za>
2021-09-10 15:11:33 +0200

    updates

--- script1.sh
+++ script1.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=compsci
 #SBATCH --partition=ada
 #SBATCH --nodes=1 --ntasks=40
-#SBATCH --time=2-2:00:00
-#SBATCH --job-name="MyAUTOML4TPJob"
+#SBATCH --time=5-2:00:00
+#SBATCH --job-name="NewAUTOML4TPJob"
 #SBATCH --mail-user=lwsada002@myuct.ac.za
 #SBATCH --mail-type=BEGIN,END,FAIL
 
