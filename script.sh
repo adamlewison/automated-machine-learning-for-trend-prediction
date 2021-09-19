dbf13a4843aa95a23feb7f1039da18b59e919b8b
Adam Lewison <lwsada002@myuct.ac.za>
2021-09-19 22:26:42 +0200

    .

--- script.sh
+++ script.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=compsci
 #SBATCH --partition=ada
 #SBATCH --nodes=1 --ntasks=40
-#SBATCH --time=2-2:00:00
-#SBATCH --job-name="MyAUTOML4TPJob"
+#SBATCH --time=3-0:00:00
+#SBATCH --job-name="NASDAQHigher"
 #SBATCH --mail-user=lwsada002@myuct.ac.za
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source venv/bin/activate
-python exp.py
+python exp.py NASDAQ
--- script1.sh
+++ script1.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #SBATCH --account=compsci
-#SBATCH --partition=gpuo
+#SBATCH --partition=ada
 #SBATCH --nodes=1 --ntasks=40
 #SBATCH --time=5-2:00:00
-#SBATCH --job-name="NewAUTOML4TPJob"
+#SBATCH --job-name="DE_STX40"
 #SBATCH --mail-user=lwsada002@myuct.ac.za
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 source venv/bin/activate
-python exp1.py
+python exp1.py STX40
